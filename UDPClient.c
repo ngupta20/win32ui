c7ce4c72f162d9a78ce02e062ce12448353e29a8
Nandish Gupta <nandishgupta@live.com>
2021-03-07 13:06:36 -0500

    first commit

---
+++ UDPClient.c
@@ -0,0 +1,34 @@
+#include <winsock2.h>
+#include <stdio.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define max_receive 1024
+#define port 8888
+#define ip_type AF_INET
+#define socket_type SOCK_DGRAM
+#define protocol IPPROTO_UDP
+#define address INADDR_ANY
+
+int main() {
+
+	WSADATA initialize_data;
+	SOCKET client;
+	SOCKADDR_IN server, SrcInfo;
+	char *message[max_receive];
+
+	WSAStartup(MAKEWORD(2,2), &initialize_data);
+
+	client = socket(ip_type, socket_type, protocol);
+
+	server.sin_family = ip_type;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	while(1) {
+		gets(message, sizeof(message));
+		sendto(client, message, max_receive, 0, &server, sizeof(server));
+	}
+
+	return 0;
+}
---
+++ UDPServer.c
@@ -0,0 +1,42 @@
+#include <winsock2.h>
+#include <stdio.h>
+#include <string.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define port 8888
+#define max_receive 1024
+#define ip_type AF_INET
+#define socket_type SOCK_DGRAM
+#define protocol IPPROTO_UDP
+#define address INADDR_ANY
+
+int main() {
+
+	WSADATA initialize_data;
+	SOCKADDR_IN server, client;
+	SOCKET receiver;
+	char buffer[max_receive];
+	int length, n;
+
+	WSAStartup(MAKEWORD(2,2), &initialize_data);
+
+	receiver = socket(ip_type, socket_type, protocol);
+
+	server.sin_family = ip_type;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(address);
+
+	bind(receiver, &server, sizeof(server));
+
+	getsockname(receiver, &server, sizeof(server));
+
+	length = sizeof(client);
+
+	while (1) {
+		n = recvfrom(receiver, buffer, max_receive, 0, &client, &length);
+		printf("%s\n", buffer);
+	}
+
+	return 0;
+}
